2125aa774b1c5329c379089d93386cd75e722048
Satyam <satyamworks20@gmail.com>
2023-11-17 12:38:02 +0530

    changes done

--- game.js
+++ game.js
@@ -9,3 +9,144 @@
 // Iteration 6: Making the Operators (button) functional
 
 // Iteration 7: Making Timer functional
+
+const number1box = document.getElementById('number1')
+const number2box = document.getElementById('number2')
+const number3box = document.getElementById('number3')
+const plus=document.getElementById('plus')
+const minus=document.getElementById('minus')
+const multiply=document.getElementById('mul')
+const divide=document.getElementById('divide')
+const modulus=document.getElementById('modulus')
+var number1=Math.round(Math.random()*100)
+var number2=Math.round(Math.random()*100)
+
+
+var numb3;
+
+
+function randomise(){
+             number1=Math.round(Math.random()*100)
+             number2=Math.round(Math.random()*100)
+             
+             if(number1<number2){
+               var temp = number1;
+               number1=number2
+               number2=number1
+             }
+
+            let operator=Math.round(Math.random()*5)
+            console.log(operator)
+
+            switch (operator){
+                case 1:
+                    numb3=number1+number2
+                    break;
+                case 2:
+                    numb3=number1-number2
+                    break;
+                case 3:
+                    numb3=number1*number2
+                    break;
+                case 4:
+                    numb3=Math.round(number1/number2)
+                    break;
+                case 5:
+                    numb3=number1%number2
+                    break;
+                case 0:
+                    randomise()                
+            }
+
+            number1box.innerText=number1
+            number2box.innerText=number2
+            number3box.innerText=numb3
+            console.log(number1,number2,numb3)
+            
+}
+randomise()
+
+var score=0;
+
+plus.addEventListener('click',()=>{
+    if(numb3==number1+number2){
+                score++
+                randomise()
+                resetTime(timerId)
+            }
+            else{
+                location.href='gameover.html?score='+score
+            }
+})
+minus.addEventListener('click',()=>{
+    if(numb3==number1-number2){
+        score++
+        randomise()
+        resetTime(timerId)
+    }
+    else{
+        location.href='gameover.html?score='+score
+    }
+
+})
+multiply.addEventListener('click',()=>{
+    if(numb3==number1*number2){
+        score++
+        randomise()
+        resetTime(timerId)
+    }
+    else{
+        location.href='gameover.html?score='+score
+    }
+})
+divide.addEventListener('click',()=>{
+    if(numb3==Math.round(number1/number2)){
+        score++
+        randomise()
+        resetTime(timerId)
+    }
+    else{
+        location.href='gameover.html?score='+score
+    }
+})
+modulus.addEventListener('click',()=>{
+    if(numb3==number1%number2){
+        score++
+        randomise()
+        resetTime(timerId)
+    }
+    else{
+        location.href='gameover.html?score='+score
+    }
+})
+var timer=document.getElementById('timer')
+var timerId
+var time =20
+
+function showtimer(){
+    var time=20
+    timer.innerText=time
+    timerId=setInterval(()=>{
+        time--
+        if(time==0){
+            location.href='gameover.html?score='+score
+        }
+        timer.innerText=time
+    },1000
+    
+    )
+
+}
+
+function resetTime(xyz){
+    clearInterval(xyz)
+    showtimer()
+}
+
+showtimer()
+
+
+
+
+
+
--- score.js
+++ score.js
@@ -1,1 +1,12 @@
 // Iteration 8: Making scoreboard functional
+
+var score=document.getElementById('score-board')
+var urlParams = new URLSearchParams(window.location.search)
+console.log(urlParams)
+console.log(window.location.search)
+var show = urlParams.get("score");
+var playagain = document.getElementById('play-again-button')
+playagain.addEventListener('click',()=>{
+    location.href="index.html"
+})
+score.innerText=show
